e6b2f7b8791ecb9db52ef3e65e6ce02174dab782
John <johnwderrick@gmail.com>
2026-08-04 20:19:50 +0100

    Initial Commit

---
+++ Football Manager/Football_ManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Football_ManagerApp.swift
+//  Football Manager
+//
+//  Created by John Derrick on 04/08/2026.
+//
+
+import SwiftUI
+
+@main
+struct Football_ManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Football Manager/SpinComponent.swift
@@ -0,0 +1,13 @@
+//
+//  SpinComponent.swift
+//  Football Manager
+//
+//  Created by John Derrick on 04/08/2026.
+//
+
+import RealityKit
+
+/// A component that spins the entity around a given axis.
+struct SpinComponent: Component {
+    let spinAxis: SIMD3<Float> = [0, 1, 0]
+}
